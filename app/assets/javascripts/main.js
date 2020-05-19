831e53dffcc36b3080312bd0cc9183ae0ce22ee8
hirokiaihara <hiro0818012359@gmail.com>
2020-05-19 13:32:53 +0900

    ヒーローのスクロール検知

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,5 +1,68 @@
 document.addEventListener('DOMContentLoaded', function() {
   new MobileMenu();
-  const hero = new HeroSlider('.swiper-container');
-  hero.start();
+  new Main();
 });
+
+class Main {
+  constructor() {
+    this.header = document.querySelector('header');
+    this._observers = [];
+    this._init();
+  }
+
+  set observers(val) {
+    this._observers.push(val);
+  }
+
+  get observers() {
+    return this._observers;
+  }
+
+  _init() {
+    this.hero = new HeroSlider('.swiper-container');
+    Pace.on('done', this._paceDone.bind(this));
+  }
+
+  _paceDone() {
+    this._scrollInit();
+  }
+
+  _toogleSlideAnimation(el, inview) {
+    if(inview) {
+      this.hero.start();
+    } else {
+      this.hero.stop();
+    }
+  }
+
+  _navAnimation(el, inview) {
+    if(inview) {
+      this.header.classList.remove('triggered');
+    } else {
+      this.header.classList.add('triggered');
+    }
+  }
+
+  _inviewAnimation(el, inview) {
+    if(inview) {
+      el.classList.add('inview');
+    } else {
+      el.classList.remove('inview');
+    }
+  }
+
+  _textAnimation = function(el, isIntersecting) {
+    if(isIntersecting) {
+      const ta = new TextAnimation(el);
+      ta.animate();
+    }
+  }
+
+  _scrollInit() {
+    this.observers = new ScrollObserver('.nav-trigger', this._navAnimation.bind(this), {once: false});
+    this.observers = new ScrollObserver('.swiper-container', this._toogleSlideAnimation.bind(this), {once: false});
+    this.observers = new ScrollObserver('.appear', this._inviewAnimation.bind(this), {once: false});
+    this.observers = new ScrollObserver('.animate-title', this._textAnimation);
+
+  }
+}
