645301a0b4d59f30523961b4489f53b88e71361c
hirokiaihara <hiro0818012359@gmail.com>
2020-05-22 22:07:27 +0900

    ルーティングとコントローラーの記述

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,5 +2,6 @@ class PostsController < ApplicationController
   
   def index
     @plays = Play.includes(:materials, :works)
+    @recipes = Recipe.includes(:foods, :makes)
   end
 end
---
+++ app/controllers/recipes_controller.rb
@@ -0,0 +1,53 @@
+class RecipesController < ApplicationController
+  before_action :set_recipe, except: [:index, :new, :create]
+
+  def index
+  end
+
+  def new
+    @recipe = Recipe.new
+    @recipe.foods.new
+    @recipe.makes.new
+  end
+
+  def create
+    @recipe = Recipe.create(recipe_params)
+    if @recipe.save
+      flash[:notice] = "投稿しました"
+      redirect_to root_path
+    else
+      flash[:alert] = "投稿できません"
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @recipe.update(recipe_params)
+      flash[:notice] = "更新しました"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:recipe_title, :recipe_image, :recipe_introduction, :recipecategory_id, foods_attributes: [:food_name, :quantity, :_destroy, :id], makes_attributes: [:make_image, :make_text, :_destroy, :id]).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
+  end
+end
