139248f052f52071215f5d46eca9c2f0ba3e2409
hirokiaihara <hiro0818012359@gmail.com>
2020-05-26 19:26:12 +0900

    マイページの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :show_plays, :show_recipes]
 
   def show
+    @plays = @user.plays.order('created_at DESC').limit(4)
+    @recipes = @user.recipes.order('created_at DESC').limit(4)
+  end
+
+  def show_plays
+    @plays = @user.plays.order('created_at DESC').page(params[:page]).per(12)
+  end
+
+  def show_recipes
+    @recipes = @user.recipes.order('created_at DESC').page(params[:page]).per(12)
+  end
+
+  def myrecipes
+    @likes = Like.where(user_id: current_user.id).order('created_at DESC').page(params[:page]).per(8)
+    @myrecipes = Myrecipe.where(user_id: current_user.id).order('created_at DESC').page(params[:page]).per(8)
   end
 
   private
