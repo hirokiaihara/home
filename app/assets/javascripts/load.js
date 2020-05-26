be67f389fc49e9bf06e48ee80bd71cd97b7364bd
hirokiaihara <hiro0818012359@gmail.com>
2020-05-27 07:56:11 +0900

    投稿編集時にinvalidされた時のjsを有効にする為の記述

--- app/assets/javascripts/load.js
+++ app/assets/javascripts/load.js
@@ -14,7 +14,8 @@ document.addEventListener("turbolinks:load", function() {
   //play投稿・編集時にnew
   const play_id = $('.play_id').val();
   const EditPlayUrl = `/plays/${play_id}/edit`;
-  if (url == "/plays/new" ||( url == "/plays" && play_id != null )|| url == EditPlayUrl) {
+  const InvalidEditPlayUrl = `/plays/${play_id}`;
+  if ( url == "/plays/new" || ( url == "/plays" && play_id != null ) || url == EditPlayUrl　|| url == InvalidEditPlayUrl ) {
     new PreviewImage('.playImg-file', '.playImg-preview');
     new AddNewFile('.material-container', '.material-textGroup', '.material-addBtn', '.material-removeBtn', '.material-alert');
     new AddNewFile('.work-container', '.work-fileGroup', '.work-addBtn', '.work-removeBtn', '.work-alert');
@@ -29,8 +30,9 @@ document.addEventListener("turbolinks:load", function() {
   }
   //recipe投稿・編集時にnew
   const recipe_id = $('.recipe_id').val();
-  const EditrecipeUrl = `/recipes/${recipe_id}/edit`;
-  if (url == "/recipes/new" || (url == "/recipes" && recipe_id != null ) || url == EditrecipeUrl) {
+  const EditRecipeUrl = `/recipes/${recipe_id}/edit`;
+  const InvalidEditRecipeUrl = `/recipes/${recipe_id}`;
+  if ( url == "/recipes/new" || (url == "/recipes" && recipe_id != null ) || url == EditRecipeUrl || url == InvalidEditRecipeUrl ) {
     new PreviewImage('.recipeImg-file', '.recipeImg-preview');
     new AddNewFile('.food-container', '.food-textGroup', '.food-addBtn', '.food-removeBtn', '.food-alert');
     new AddNewFile('.make-container', '.make-fileGroup', '.make-addBtn', '.make-removeBtn', '.make-alert');
--- app/assets/javascripts/scripts.js/previewImage.js
+++ app/assets/javascripts/scripts.js/previewImage.js
@@ -1,14 +1,7 @@
 //ユーザー編集・play投稿&編集・recipe投稿&編集のプレビュー表示機能
 class PreviewImage {
   constructor(file, preview) {
-    this.file = file;
-    this.preview = preview;
-    this._readImage();
-  }
-
-  _readImage() {
-    var preview = this.preview;
-    document.querySelector(this.file).addEventListener('change', function(e) {
+    document.querySelector(file).addEventListener('change', function(e) {
       var file = e.target.files[0];
       var fileReader = new FileReader();
       fileReader.onload = function() {
