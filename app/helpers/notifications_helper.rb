24ad36fb47c98cc7d63e94f47319eba8a0e47bd2
hirokiaihara <hiro0818012359@gmail.com>
2020-06-01 12:17:30 +0900

    コメントの追加

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -1,5 +1,5 @@
 module NotificationsHelper
-
+  # 通知文を場合分け
   def notification_form(notification)
     @playcomment = nil
     @recipecomment = nil
@@ -25,7 +25,7 @@ module NotificationsHelper
         visiter + "が" + your_group + "にメッセージを送りました"
     end
   end
-
+  # 未確認の通知がある時にヘッダーでお知らせする
   def unchecked_notifications
     @uncheck = current_user.passive_notifications.where(checked: false)
   end
