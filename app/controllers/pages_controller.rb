b7ea11e542ca3a8af49998521f9d779153901f62
hirokiaihara <hiro0818012359@gmail.com>
2020-06-01 12:16:52 +0900

    コメントの追加と不要な記述の削除

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
   def index
+    # 新着投稿を表示
     @plays = Play.includes(:materials, :works).order('plays.created_at desc').page(params[:page]).per(8)
     @recipes = Recipe.includes(:foods, :makes).order('recipes.created_at desc').page(params[:page]).per(6)
+    # ランキングを表示
     @ranked_plays = Play.includes(:materials, :works).create_ranks
     @ranked_recipes = Recipe.includes(:foods, :makes).create_ranks
   end
 
-  def show
-    @likes = Like.includes(:plays).order('likes.created_at desc').page(params[:page]).per(12)
-  end
 end
