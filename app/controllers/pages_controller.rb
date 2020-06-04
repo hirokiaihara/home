d5b5fda64adde0fbaa49c474ef16251bd8c10803
hirokiaihara <hiro0818012359@gmail.com>
2020-06-04 09:30:26 +0900

    新着投稿の表示枚数を変更

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,7 @@
 class PagesController < ApplicationController
   def index
     # 新着投稿を表示
-    @plays = Play.includes(:materials, :works).order('plays.created_at desc').page(params[:page]).per(8)
+    @plays = Play.includes(:materials, :works).order('plays.created_at desc').page(params[:page]).per(4)
     @recipes = Recipe.includes(:foods, :makes).order('recipes.created_at desc').page(params[:page]).per(6)
     # ランキングを表示
     @ranked_plays = Play.includes(:materials, :works).create_ranks
